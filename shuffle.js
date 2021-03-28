f927a9d5950aaa9fd5a1304360ae156d2a59aa71
Adina Rubinoff <adina.elisheva@gmail.com>
2021-03-28 00:36:31 -0400

    adding shuffle

---
+++ shuffle.js
@@ -0,0 +1,44 @@
+if (process.argv.length < 3) {
+  console.log('usage: node shuffle.js [TOSHUFFLE] [CONSTRAINTS...]');
+  console.log('where: \n  - TOSHUFFLE is any string');
+  console.log('  - CONSTRAINTS are any number of letter-index requirements such as S6 or A0');
+  return;
+}
+
+let seen = {};
+const constraints = [];
+for (let i = 3; i < process.argv.length; i++) {
+  let constraint = process.argv[i].split('');
+  constraints.push([
+    constraint[0],
+    Number(constraint[1]),
+  ]);
+}
+
+function printPermutation(curPerm, left) {
+  if (left.length === 0) {
+    if (!seen[curPerm]) {
+      seen[curPerm] = true;
+      let allowed = true;
+      for (let constraint of constraints) {
+        if (curPerm[constraint[1]] !== constraint[0]) {
+          allowed = false;
+          break;
+        }
+      }
+      if (allowed) {
+        console.log(curPerm);
+      }
+    }
+    return;
+  }
+  for (let i = 0; i < left.length; i++) {
+    let nextPerm = curPerm.slice();
+    let nextLeft = left.slice();
+    nextPerm += left[i];
+    nextLeft = nextLeft.substring(0,i) + nextLeft.substring(i+1);
+    printPermutation(nextPerm, nextLeft);
+  }
+}
+
+printPermutation('', process.argv[2]);
